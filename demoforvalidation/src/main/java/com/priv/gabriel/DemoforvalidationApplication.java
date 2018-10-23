ba7bef5fb9d4a947cb762d2aa0fbdbf419ea9abe
gabriel <youarephoenix@163.com>
2018-10-23 17:11:15 +0800

    init

---
+++ demoforvalidation/src/main/java/com/priv/gabriel/DemoforvalidationApplication.java
@@ -0,0 +1,12 @@
+package com.priv.gabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoforvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoforvalidationApplication.class, args);
+	}
+}
